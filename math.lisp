5bfc1f30a8bb66b3f1ec7098e570e394eaa31a80
Alexander Koch <kochalexander@gmx.net>
2016-04-10 14:12:37 +0200

    Expand standard library

--- math.lisp
+++ math.lisp
@@ -8,6 +8,13 @@
 (fun #(square x) #(* x x))
 (fun #(inc x) #(+ x 1))
 (fun #(dec x) #(- x 1))
+(fun #(neg x) #(- 0 x))
+(fun #(abs x)
+	#(if (< x 0)
+		#(* x (neg 1))
+		#(x)
+	)
+)
 
 (fun #(deg2rad x) #(* x (/ pi 180)))
 (fun #(rad2deg x) #(* x (/ 180 pi)))
--- std.lisp
+++ std.lisp
@@ -11,13 +11,23 @@
 ))
 
 ; First and second element of a list
-(fun #(fst ls) #(head ls))
-(fun #(snd ls) #(head (tail ls)))
+(fun #(fst ls) #(eval (head ls)))
+(fun #(snd ls) #(eval (head (tail ls))))
 
-; List size
+; List operations
 (fun #(len l)
 	#(if (== l nil)
 		#(0)
 		#(+ 1 (len (tail l)))
+	)
+)
+
+(fun #(contains l e)
+	#(if (== l nil)
+		#(false)
+		#(if (== (fst l) e)
+			#(true)
+			#(contains (tail l) e)
+		)
 	)
 )
