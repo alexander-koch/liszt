59583573dded82c7a506c7c66e74d49f1bd97b92
Alexander Koch <kochalexander@gmx.net>
2016-04-22 11:27:06 +0200

    Update indentation in the standard libraries

--- std.lisp
+++ std.lisp
@@ -6,9 +6,10 @@
 (var #false 0)
 
 ; Function varinition formals and body as parameters
-(var #fun (lambda #(f b)
-	#(var (head f) (lambda (tail f) b))
-))
+(var #fun 
+  (lambda #(f b)
+    #(var (head f) 
+      (lambda (tail f) b))))
 
 ; First and second element of a list
 (fun #(fst ls) #(eval (head ls)))
@@ -16,27 +17,20 @@
 
 ; List length
 (fun #(len l)
-	#(if (== l nil)
-		#(0)
-		#(+ 1 (len (tail l)))
-	)
-)
+  #(if (== l nil)
+    #(0)
+    #(+ 1 (len (tail l)))))
 
 ; List contains element
 (fun #(contains l e)
-	#(if (== l nil)
-		#(false)
-		#(if (== (fst l) e)
-			#(true)
-			#(contains (tail l) e)
-		)
-	)
-)
+  #(if (== l nil)
+    #(false)
+    #(if (== (fst l) e)
+      #(true)
+      #(contains (tail l) e))))
 
 ; List index
 (fun #(index l i)
-	#(if (== i 0)
-		#(fst l)
-		#(index (tail l) (- i 1))
-	)
-)
+  #(if (== i 0)
+    #(fst l)
+    #(index (tail l) (- i 1))))
